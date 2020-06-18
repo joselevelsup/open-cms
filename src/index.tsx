18402db113fa9a2d9e5b4778cfa896be4b32ecb5
joselevelsup <hipnotickid12@gmail.com>
2020-06-17 23:05:02 -0400

    renamed files and some variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,59 @@
 import * as React from "react";
-import * as ReactDom from "react-dom";
+import { useRoutes } from "hookrouter";
+import { ThemeProvider } from "styled-components";
+import CmsPage from "./components/cmspage";
+import { slugify } from "./util";
 
-import AdminCMS from "./app";
+export type CmsRoute = {
+	name: string,
+	apiRoute: string,
+}
 
-ReactDom.render(
-		<AdminCMS />
-	, document.getElementById("root"));
+interface MainAppProps {
+	apiAddress?: string,
+	routes: [CmsRoute],
+	theme: any, //TODO: be specific on what the user can customize
+	logo: any,
+	components: { name: string, slug: string, component: React.ComponentType }[]
+}
+
+function OpenCms(props: MainAppProps) {
+	const { routes, theme, logo, apiAddress, components } = props;
+	
+	let remappedRoutes = {};
+
+	for(let r in routes){
+		remappedRoutes[`/admin/${slugify(routes[r].name)}`] = () =>(
+		<ThemeProvider theme={theme}>
+			<CmsPage 
+				otherRoutes={routes} 
+				apiRoute={apiAddress ? `${apiAddress}${routes[r].apiRoute}` : routes[r].apiRoute} 
+				logo={logo}
+				customComponents={components}
+			/>
+		</ThemeProvider>
+		) 
+	}
+
+	const router = useRoutes(remappedRoutes);
+
+	return router;
+}
+
+OpenCms.defaultProps = {
+	routes: [
+		{
+			name: "home page",
+			apiRoute: "/home-page",
+		},
+		{
+			name: "about page",
+			apiRoute: "/about-page",
+		}
+	],
+	theme: {},
+	logo: null,
+	components: []
+};
+
+export default OpenCms;
---
+++ src/render.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+
+import OpenCms from "./";
+
+ReactDom.render(
+		<OpenCms />
+	, document.getElementById("root"));
